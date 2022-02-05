172091b6253792dc06340b12d43705777c24257c
Benjiman Sutton <benjimansutton@gmail.com>
2022-02-05 13:02:16 +0000

    Link first question with the output menu

--- run.py
+++ run.py
@@ -1,4 +1,59 @@
 
+#Question one story opener function
+def opener_question():
+    """
+    Function for question one to allow the user to,
+    select their first path
+    """
+
+    print("You spot an abandoned Warship in the sea by your house,\nyou decide to venture towards the ship.\nYou spot there is no Captain or Crew aboard the ship.\n")
+    print("You get in your rowing boat and head towards the Warship.\nYou see a ladder hanging over the side of the ship,\nswaying in the wind.")
+    question_one = input("What do you do?:\n a: Climb aboard the ship alone?\nb: Report the ship to the coast guard?\nc: Call your friends to go abouard with you?\n").lower().strip()
+
+    if question_one == "a":
+        print("You have selected - Option a - Climb aboard the ship alone?\n")
+        aboard_the_ship()
+    elif question_one == "b":
+        print("You have selected - Option b - Report the ship to the coast guard?")
+        see_a_figure()
+    elif question_one == "c":
+        print("You have selected - Option c - Call your friends to go abouard with you?")
+        friends_arrive()
+
+#To get aboard the ship function question two
+def aboard_the_ship():
+    """
+    Function for question two to show the path to,
+    aboard the ship
+    """
+    print("go aboard the ship")
+
+
+#Question number three function
+def see_a_figure():
+    """
+    Function for question three to see a figure on the
+    ship
+    """
+    print("see a figure activated")
+
+#Question number four
+def friends_arrive():
+    """
+    Function to have your friends arrive to carry on the 
+    explouration of the ship
+    """
+    print("friends arrive activated")
+
+#Question number five
+def coastguard():
+    """
+    Function to have the coastguard assist in seeing the ship,
+    to report it and also to find if your allowed to board the ship
+    """
+    print("coastguard activated")
+
+
 #Quit the game function to allow the Player to leave
 def quit_game():
     """
@@ -6,7 +61,13 @@ def quit_game():
     at multiple points including the menu and during
     game play
     """
+    i = "You have decided to Quit the game, Thanks for playing.\n"
+    while True:
+        print(i)
+    
     print("quit game activated")
+
+
 
 #Play game function to start into the questions
 def play_game():
@@ -14,18 +75,21 @@ def play_game():
     Function for game play to start and push the player
     towards the first question
     """
-    print("play game activated")
+    print("You have selected Play the Game.\n")
+    opener_question()
 
-#Play game function to start into the questions
+#Read game function to explain to the Player the backstory of the game
 def read_game_story():
     """
-    Function for game play to start and push the player
-    towards the first question
+    Function for the backstory of the game which once has been printed will
+    send the Player back to the menu
     """
     print("Warship is a text based game that brings you to the shore line by your house,\nwhen you arrive at the waters edge, you see a World War II Warship.")
     print("This ship looks as though it is abandoned, and it sends shivers down your spine.\nYou decide to go towards the vessel to see if its real or your imagination.")
-    print("When you get to the ship, thr story takes you down various paths and as the story unfolds you become,\nmore intrested in what the ship has to offer.")
-    print(input("What would you like to do next?"))
+    print("When you get to the ship, thr story takes you down various paths and as the story\nunfolds you become, more intrested in what the ship has to offer.")
+    print(menu(input("What would you like to do next?")))
+
+
 #Opening page for game
 def game_intro():
     """
@@ -35,6 +99,8 @@ def game_intro():
     print("Welcome to Warship, the game where your choices can change your life.\n")
     print("Warship is designed for you to move through the different paths\n")
     menu()
+
+
 
     #Menu for the Player to select their path
 def menu():
@@ -62,7 +128,9 @@ def menu():
         game_intro()
 
 
+
 game_intro()
+
 
 
 
@@ -74,42 +142,6 @@ game_intro()
 
 
 
-#Question one story opener function
-def opener_question():
-    """
-    Function for question one to allow the user to,
-    select their first path
-    """
-
-
-#To get aboard the ship function question two
-def aboard_the_ship():
-    """
-    Function for question two to show the path to,
-    aboard the ship
-    """
-
-
-#Question number three function
-def see_a_figure():
-    """
-    Function for question three to see a figure on the
-    ship
-    """
-
-#Question number four
-def friends_arrive():
-    """
-    Function to have your friends arrive to carry on the 
-    explouration of the ship
-    """
-
-#Question number five
-def coastguard():
-    """
-    Function to have the coastguard assist in seeing the ship,
-    to report it and also to find if your allowed to board the ship
-    """
 
 #Question number six
 def inside_the_ship():
