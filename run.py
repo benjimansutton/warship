59c1198ac74fff312ea21034cff35e57e711d8a7
Benjiman Sutton <benjimansutton@gmail.com>
2022-02-05 10:56:29 +0000

    See above commit

--- run.py
+++ run.py
@@ -2,5 +2,22 @@
 def game_intro():
     """
     Function to introduce the player to the game,
-    the story opens in 5 lines"""
+    the story opens in 5 lines
+    """
+
+#Menu for the Player to select their path
+def menu():
+    """
+    Function to recieve the Player path,
+    to start the game, learn more about the story,
+    or quit the game
+    """
+
+#Quit the game function to allow the Player to leave
+def quit_game():
+    """
+    Function to allow the Player to leave the game,
+    at multiple points including the menu and during
+    game play
+    """
 
